c59fca7dcbf7154869b24fadaad915ba469a08d8
Vivek S. Lakhani <88651044+vivek-lakhani-1@users.noreply.github.com>
2024-02-07 15:37:45 +0530

    Updation of User_Registration APIs

--- User/admin.py
+++ User/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import User_Registration
+from .models import User_Registration,User_Credentials
 
 class UserRegistrationAdmin(admin.ModelAdmin):
     list_display = ('User_Id', 'Full_Name', 'Username', 'Email_Address', 'Phone_Number', 'Date_Of_Registration')
     search_fields = ['User_Id', 'Full_Name', 'Username', 'Email_Address', 'Phone_Number']
-    #list_filter = ('Organization_Name', 'State', 'District', 'City', 'ABC_ID')
+    list_filter = ('Organization_Name', 'State', 'District', 'City', 'ABC_ID')
 
+class UserCredentialsAdmin(admin.ModelAdmin):
+    list_display = ('User_Id','User_Name','User_Email')
+    list_filter = ('User_Id','User_Name','User_Email')
+admin.site.register(User_Credentials,UserCredentialsAdmin)
 admin.site.register(User_Registration, UserRegistrationAdmin)
--- User/views.py
+++ User/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework import response
+from rest_framework.response import Response
+from .serializer import UserCredentialsSerializer
 
 @api_view(['POST'])
 def User_Registration(request):
+    serializer = UserCredentialsSerializer(data=request.data)
     
-    return response.Response({'Status':200,'Message':'Data Received'})
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
